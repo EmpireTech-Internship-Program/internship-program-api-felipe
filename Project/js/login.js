eb53f31200a4eada687057a780b6a64a3c55f072
FelipeAVSantos <felipealef5524@gmail.com>
2024-05-27 21:41:15 -0300

    27/05
    Criação de array para validação de login

--- Project/js/login.js
+++ Project/js/login.js
@@ -1,13 +1,32 @@
-function logar() {
+const users = [
+    {
+        username: "felipe@gmail.com",
+        password: "felipe"
+    },
+    {
+        username: 'carlos@gmail.com',
+        password: 'carlos'
+    },
+    {
+        username: 'ellen@gmail.com',
+        password: 'ellen'
+    }
+];
 
-var login = document.getElementById('email').value;
-var password = document.getElementById('password').value;
-  
-if(login == "admin" && password == "admin"){
-    alert('Sucess');
-    location.href = "/views/home.html"
-}else{
-    alert('wrong password');
+function validateLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var user = users.find(function(user) {
+        return user.username === username && user.password === password;
+    });
+   
+    if (user) {
+        alert("Successful login!");
+        location.href = "/views/home.html"
+    } else {
+        alert("Incorrect username or password. Try again.");
+    }
 }
 
-}
+
